97f9f745570786bebc41223021ffa7e0791b4dd6
Bunpot Klaisorn <103012749+noteyn51@users.noreply.github.com>
2023-01-26 11:17:08 +0700

    updates

--- src/Help.js
+++ src/Help.js
@@ -25,7 +25,6 @@ import CalendarConfig from './calendar/calendar';
 import AgendaScreen from './calendar/agendar';
 // import PDFExample from './pdf/pdfscreen';
 // import PDFExample from './pdf/pdfscreen';
-// import PDFExample from './pdf/pdfscreen';
 
 
 function Help() {
